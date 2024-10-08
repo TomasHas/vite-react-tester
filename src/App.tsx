c0afa614842436cf6bd25f3b7ce9c51df56b68e5
Tomas Hastrup <thastrup@gmail.com>
2024-10-08 01:59:59 -0300

    tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-
+import Tasks from "./components/tasks/Tasks";
 function App() {
   return (
     <div className=" bg-rose-700  flex flex-col justify-center items-center">
       <div className="flex flex-col items-center   mt-20 md:w-screen lg:w-[60%]">
         <p className=" text-4xl">Pomodoro</p>
-
+        <Tasks />
         <div> </div>
       </div>{" "}
     </div>
